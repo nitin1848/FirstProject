4f540a3e6c5faf93e69a5bb1484301f1649f6d8d
68060_nitin <nitinnavkhare1995@gmail.com>
2024-04-02 10:12:08 +0530

    bank managBmeant project

---
+++ Bank Management System/src/com/braindata/bankmanagement/client/Test.java
@@ -0,0 +1,17 @@
+package com.braindata.bankmanagement.client;
+import com.braindata.bankmanagement.service.Rbi;
+import com.braindata.bankmanagement.serviceImpl.Sbi;
+
+public class Test
+{
+	public static  void main(String[] args)
+	{
+		Rbi bank=new Sbi();
+		//Display proper msg for calling methods.
+		//As per user choice perform bank operation using switch case
+		switch(...)
+		{
+			//only call Sbi methods here
+		}
+    	 }
+}
---
+++ Bank Management System/src/com/braindata/bankmanagement/serviceImpl/Sbi.java
@@ -0,0 +1,38 @@
+package com.braindata.bankmanagement.serviceImpl;
+
+import com.braindata.bankmanagement.service.Rbi;
+
+public class Sbi implements Rbi
+{
+
+	@Override
+	public void createAccount() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void displayAllDetails() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void depositeMoney() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void withdrawal() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void balanceCheck() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
